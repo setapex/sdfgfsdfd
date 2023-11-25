742ffc60fb90bdef9b0eae99a200f07564809689
setapex <103444113+setapex@users.noreply.github.com>
2023-11-26 01:04:55 +0300

    auth

--- zay/settings.py
+++ zay/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'aft',
     'multiselectfield',
     'django_filters',
 ]
@@ -132,3 +133,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- zay/urls.py
+++ zay/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from zay import views
 from zay.views import ProductListView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', include('aft.urls')),
     path('about', views.about, name='about'),
     path('shop', ProductListView.as_view(), name='shop'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('admin/', admin.site.urls),
-    path('reg', views.reg, name='re'),
 ]
